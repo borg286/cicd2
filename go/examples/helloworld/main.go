6a97e96165f44905baa56c3ba894bd7750ff88ba
Brian Matthews <mrmath@google.com>
2026-04-30 05:56:06 +0000

    Update main.go to start a blocking HTTP server

--- go/examples/helloworld/main.go
+++ go/examples/helloworld/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World from Gitea Automation!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello World from Gitea Automation!")
+	})
+	fmt.Println("Starting server on :8080")
+	http.ListenAndServe(":8080", nil)
 }
