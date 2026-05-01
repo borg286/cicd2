80d6855740d30bc88b0113b5c67cf2d39d840356
Brian Matthews <mrmath@google.com>
2026-05-01 02:20:42 +0000

    Clean up debugging noise from workflow and script

--- go/examples/helloworld/build-test-push.sh
+++ go/examples/helloworld/build-test-push.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -e
-set -x
-
-trap 'echo "Script exited with status $?"' EXIT
-
 bazel build //go/examples/helloworld:helloworld_chart.push
 bazel run //go/examples/helloworld:helloworld_chart.push
