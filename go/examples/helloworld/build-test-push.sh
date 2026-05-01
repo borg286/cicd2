dd31d20b1524f06fbf9a4fb8a694603d306ecb15
Brian Matthews <mrmath@google.com>
2026-05-01 02:01:20 +0000

    Add debugging to workflow and script

--- go/examples/helloworld/build-test-push.sh
+++ go/examples/helloworld/build-test-push.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
+set -x
+
+trap 'echo "Script exited with status $?"' EXIT
+
 bazel build //go/examples/helloworld:helloworld_chart.push
 bazel run //go/examples/helloworld:helloworld_chart.push
