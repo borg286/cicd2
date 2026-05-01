c86e74710480af067fdb5d2661f322c2136e2d3b
Brian Matthews <mrmath@google.com>
2026-05-01 02:08:40 +0000

    minore tweak

--- go/examples/helloworld/main.go
+++ go/examples/helloworld/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello World from Gitea Automation!")
+		fmt.Fprintln(w, "Hello World from Gitea  Automation!")
 	})
 	fmt.Println("Starting server on :8080")
 	http.ListenAndServe(":8080", nil)
